25d4f98edaf3befa047a832439c23fbffa860b68
Alistair Turnbull <apt1002@frog>
2022-04-19 13:52:02 +0100

    Delete class `Evaluate`, replacing it with its only field.

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,17 @@
+'''
+The interpreter. Code is represented by a Table. The key '' is the
+opcode, and the remaining keys are the operands. Code is evaluated in an
+environment: a dict from str to Value. The environment provides values for
+captures and locals. Globals are constants compiled into the code, and not
+included in the environment.
+
+`ACTIONS` is the complete instruction set. It is a dict from opcode to function
+from (environment, code) to Value, where:
+ - opcode - str.
+ - environment - dict from str to Value.
+ - code - dict from str.
+'''
+
 from model import Table, Lambda, NULL
 
 def evaluate_dict(environment, dict_):
@@ -14,12 +28,13 @@ def apply(function, argument):
     '''
     assert type(function) is Lambda
     environment = {function.parameter: argument}
+    assert function.parameter not in function.captures
     environment.update(function.captures)
-    return evaluate(Evaluate(environment), function.body)
+    return evaluate(environment, function.body)
 
 ACTIONS = {
     'constant': lambda _, d: d['constant'],
-    'name': lambda e, d: e.environment[d['name'].name],
+    'name': lambda e, d: e[d['name'].name],
     'lambda': lambda e, d: Lambda(
         evaluate_dict(e, d['captures'].dict),
         d['parameter'].name,
@@ -36,21 +51,12 @@ ACTIONS = {
 
 def evaluate(environment, expression):
     '''
-     - environment - Environment.
+     - environment - dict from str to Value.
      - expression - model.Value representing code.
     '''
     assert type(expression) is Table, expression
     case = ACTIONS[expression.dict[''].name]
     return case(environment, expression.dict)
-
-class Evaluate:
-    '''
-    Usage: `Evaluate(environment)(expression)`.
-
-     - environment - dict from str to Value.
-    '''
-    def __init__(self, environment):
-        self.environment = environment
 
 def built_ins():
     return {
@@ -61,6 +67,6 @@ if __name__ == '__main__':
     from parser import  parse
     environment = built_ins()
     e = parse('fn x {fn y {x}}', environment)
-    environment['k'] = evaluate(Evaluate({}), e)
+    environment['k'] = evaluate({}, e)
     a = parse('{a: k(@foo)(@bar)}', environment)
-    print(evaluate(Evaluate({}), a))
+    print(evaluate({}, a))
